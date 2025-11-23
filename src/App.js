0f23cd4aa6d02a63750cfcc07e760383847cbbe2
asmaa <elabedasmaa586@gmail.com>
2025-11-23 16:16:01 +0200

    editing json for deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Components
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
