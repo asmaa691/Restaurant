9012dfadc5d75e8acf72d1b165036d81714ecdb7
asmaa <elabedasmaa586@gmail.com>
2025-11-24 19:46:16 +0200

    editing services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,14 +4,13 @@ export default function Services() {
   return (
     <div className="services">
       <h2 className="red">Our Services</h2>
-      <ul>
-        <li>Browse the restaurant menu with images and prices</li>
-        <li>Add items to the cart</li>
-        <li>Remove items from the cart</li>
-        <li>View total price of the order</li>
-        <li>Responsive design for desktop and mobile devices</li>
-        <li>About and Contact pages with information and support</li>
-      </ul>
+     <ul>
+  <li>Dine-in restaurant with cozy atmosphere</li>
+  <li>Takeaway and delivery options</li>
+  <li>Catering for special events</li>
+  <li>Fresh ingredients sourced daily</li>
+  <li>Customizable menu options</li>
+</ul>
     </div>
   );
 }
